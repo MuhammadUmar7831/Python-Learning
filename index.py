69f972830ce0a14023ea5eb3616520688ff656fd
Muhammad Umar <mu8494759@gmail.com>
2024-06-28 01:50:57 +0500

    day 74

--- index.py
+++ index.py
@@ -1,4 +1,25 @@
 """
+#video 74
+#Method Overriding in Python
+
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def area(self):
+        return self.x * self.y
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+rec = Shape(5, 8)
+print(rec.area())
+crc = Circle(7)
+print(crc.area())
+
 #video 73
 #Magic/Dunder Methods in Python
 
